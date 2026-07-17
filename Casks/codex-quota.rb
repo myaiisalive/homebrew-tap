9814b23e6e88b9f35eea69c3682b9298a190dce9
zph <1217287041@qq.com>
2026-07-17 16:48:19 +0800

    Update codex-quota to 0.5.3

--- Casks/codex-quota.rb
+++ Casks/codex-quota.rb
@@ -1,13 +1,13 @@
 cask "codex-quota" do
-  version "0.5.2"
+  version "0.5.3"
 
   on_arm do
     url "https://github.com/myaiisalive/codex-quota/releases/download/v#{version}/CodexQuota-#{version}-arm64.zip"
-    sha256 "7e81ff52f1e1052fcc3c237af8ff5c95a6fb793011fa8495b4733f798a56181c"
+    sha256 "41a2cbf4b71ac6d547875f8049a0475a7d03596e0b4c184907f8ecb0a2fb90cb"
   end
   on_intel do
     url "https://github.com/myaiisalive/codex-quota/releases/download/v#{version}/CodexQuota-#{version}-universal.zip"
-    sha256 "2448ace6bfd8eb884a90e9f6d7413eff082e057410d09be8664da224b15a28e0"
+    sha256 "c6b2277391d8b474ae58bf0b6027bbef5fdd254a966529c82193632f7fe02bce"
   end
 
   name "CodexQuota"
